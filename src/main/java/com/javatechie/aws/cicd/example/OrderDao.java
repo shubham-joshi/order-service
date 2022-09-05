b66da63a43e76098a19cc90173a3b764cc67ea9a
Shubham Joshi <j.shubham87@gmail.com>
2022-09-05 12:49:25 +0530

    Renamed headset to headpone

--- src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
+++ src/main/java/com/javatechie/aws/cicd/example/OrderDao.java
@@ -9,13 +9,12 @@ import java.util.stream.Stream;
 @Repository
 public class OrderDao {
 
-
     public List<Order> getOrders() {
         return Stream.of(
                 new Order(101, "Mobile", 1, 30000),
                 new Order(58, "Book", 4, 2000),
                 new Order(205, "Laptop", 1, 150000),
-                new Order(809, "headset", 1, 1799))
+                new Order(809, "headphone", 1, 1799))
                 .collect(Collectors.toList());
     }
 }
